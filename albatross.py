735e5ed1a85f1504eb3552410986942d90c8bba7
mfrankovic0 <mfrankovic34@gmail.com>
2020-12-08 20:38:42 -0500

    Update README.md

---
+++ albatross.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+
+class ListGen:
+    """
+    ListGen(erator) will search through directories for files with the 
+    specified file extensions for each corresponding function. The results will
+    be added to a dictionary along with metadata. When it is finished, the 
+    dictionaries will be dumped into a JSON file.
+    """
+
+    def __init__(self)
+
+    def find_music(self):
+    
+    def find_text_and_docs(self):
+    
+    def find_pics(self):
+
+    def find_apps(self):
+
+    def find_archives(self):
+
+    def find_videos(self):
+
+    
+
+
+
+class 
