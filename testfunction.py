d45779cbcb46c3b26abb41fc507da9d6e2a81b19
mfrankovic0 <mfrankovic34@gmail.com>
2020-12-15 21:12:25 -0500

    getting somewhere

--- testfunction.py
+++ testfunction.py
@@ -1,8 +1,23 @@
 import os
 import json
+import datetime
+
+txtlist = {}
 
 def find_txt():
-    for root, dirs, files in os.walk(".txt", topdown=False):
-        print(os.path.join(files, name))
+    for root, dirs, files in os.walk("/home/mike/Downloads/Tool/"):
+        for file in files:
+            if file.endswith((".flac", ".ape", ".mp3, ")):
+                filename, file_extension = os.path.splitext(file)
+                pathname = os.path.join(root, file)
+                try:
+                    cdate = os.stat(file).st_ctime
+                    date = datetime.fromtimestamp(cdate)
+                except FileNotFoundError:
+                    pass     
+                txtlist.update({filename: {'Path': pathname, 'files_ext': file_extension, 'Date': date}})
 
 find_txt()
+
+with open('test.json', 'w') as json_file:
+    json.dump(txtlist, json_file, sort_keys=True, indent=4)
