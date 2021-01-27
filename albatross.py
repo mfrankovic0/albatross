0ee431b8942866114c3e61018d8f387979b1d797
mfrankovic0 <mfrankovic34@gmail.com>
2021-01-27 18:16:41 -0500

    KOOOMMIIITTT

--- albatross.py
+++ albatross.py
@@ -1,8 +1,8 @@
 import os
 import json
+from datetime import date
 
 
-class ListGen:
     """
     ListGen(erator) will search through directories for files with the 
     specified file extensions for each corresponding function. The results will
@@ -10,22 +10,134 @@ class ListGen:
     dictionaries will be dumped into a JSON file.
     """
 
-    def __init__(self):
+FileTypes =
+{
+  "Images": [
+    ".jpeg",
+    ".jpg",
+    ".tiff",
+    ".gif",
+    ".bmp",
+    ".png",
+    ".bpg",
+    ".svg",
+    ".heif",
+    ".psd",
+    ".cr2",
+    ".indd"
+  ],
+  "Videos": [
+    ".avi",
+    ".flv",
+    ".wmv",
+    ".mov",
+    ".mp4",
+    ".webm",
+    ".vob",
+    ".mng",
+    ".qt",
+    ".mpg",
+    ".mpeg",
+    ".3gp",
+    ".mkv"
+  ],
+  "Documents": [
+    ".txt",
+    ".oxps",
+    ".epub",
+    ".pages",
+    ".docx",
+    ".doc",
+    ".fdf",
+    ".ods",
+    ".odt",
+    ".pwi",
+    ".xsn",
+    ".xps",
+    ".dotx",
+    ".docm",
+    ".dox",
+    ".rvg",
+    ".rtf",
+    ".rtfd",
+    ".wpd",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx",
+    ".md",
+    ".pages",
+    ".numbers",
+    ".pdf"
+  ],
+  "Archives": [
+    ".a",
+    ".ar",
+    ".arh",
+    ".tar",
+    ".tar.bz2",
+    ".tar.gz",
+    ".cpio",
+    ".tar",
+    ".gz",
+    ".rz",
+    ".7z",
+    ".rar",
+    ".xar",
+    ".zip",
+    ".xz",
+    ".pkg",
+    ".deb",
+    ".rpm"
+  ],
+  "Diskimages": [
+    ".iso",
+    ".img",
+    ".vcd",
+    ".dmg"
+  ],
+  "Audio": [
+    "flac",
+    ".ape",
+    ".aac",
+    ".aa",
+    ".aac",
+    ".dvf",
+    ".m4a",
+    ".m4b",
+    ".m4p",
+    ".mp3",
+    ".msv",
+    "ogg",
+    "oga",
+    ".raw",
+    ".vox",
+    ".wav",
+    ".wma"
+  ],
+  "Apps": [
+    ".exe",
+    ".msi"
+  ]
+}
 
-    def find_music(self):
-    
-    def find_text_and_docs(self):
-    
-    def find_pics(self):
+filelist = {}
 
-    def find_apps(self):
+def find_file(filetype, ext):
+    for root, dirs, files in os.walk("/home/mike/albatross"):
+        for file in files:
+            if file.endswith((".txt")):
+                filename, file_ext = os.path.splitext(file)
+                pathname = os.path.join(root, file)
+                try:
+                    cdate = os.stat(file).st_ctime
+                    timestamp = date.fromtimestamp(cdate)
+                    fdate = timestamp.strftime("%D")
+                except FileNotFoundError:
+                    continue 
+                filelist.update({filename: {'Path': pathname, 'File Extension': file_ext, 'Date': fdate}})
 
-    def find_archives(self):
+find_file()
 
-    def find_videos(self):
-
-    
-
-
-
-class 
+with open('test.json', 'w') as json_file:
+    json.dump(txtlist, json_file, sort_keys=True, indent=4)
--- testfunction.py
+++ testfunction.py
@@ -3,22 +3,142 @@ import json
 from datetime import date
 
 
-txtlist = {}
+FileTypes = {
+  "Images": (
+    ".jpeg",
+    ".jpg",
+    ".tiff",
+    ".gif",
+    ".bmp",
+    ".png",
+    ".bpg",
+    ".svg",
+    ".heif",
+    ".psd",
+    ".cr2",
+    ".indd"
+  ),
+  "Videos": (
+    ".avi",
+    ".flv",
+    ".wmv",
+    ".mov",
+    ".mp4",
+    ".webm",
+    ".vob",
+    ".mng",
+    ".qt",
+    ".mpg",
+    ".mpeg",
+    ".3gp",
+    ".mkv"
+  ),
+  "Documents": (
+    ".txt",
+    ".oxps",
+    ".epub",
+    ".pages",
+    ".docx",
+    ".doc",
+    ".fdf",
+    ".ods",
+    ".odt",
+    ".pwi",
+    ".xsn",
+    ".xps",
+    ".dotx",
+    ".docm",
+    ".dox",
+    ".rvg",
+    ".rtf",
+    ".rtfd",
+    ".wpd",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx",
+    ".md",
+    ".pages",
+    ".numbers",
+    ".pdf",
+    ".json"
+  ),
+  "Archives": (
+    ".a",
+    ".ar",
+    ".arh",
+    ".tar",
+    ".tar.bz2",
+    ".tar.gz",
+    ".cpio",
+    ".tar",
+    ".gz",
+    ".rz",
+    ".7z",
+    ".rar",
+    ".xar",
+    ".zip",
+    ".xz",
+    ".pkg",
+    ".deb",
+    ".rpm"
+  ),
+  "Diskimages": (
+    ".iso",
+    ".img",
+    ".vcd",
+    ".dmg"
+  ),
+  "Audio": (
+    ".flac",
+    ".ape",
+    ".aac",
+    ".aa",
+    ".aac",
+    ".dvf",
+    ".m4a",
+    ".m4b",
+    ".m4p",
+    ".mp3",
+    ".msv",
+    ".ogg",
+    ".oga",
+    ".raw",
+    ".vox",
+    ".wav",
+    ".wma"
+  ),
+  "Apps": (
+    ".exe",
+    ".msi"
+  )
+}
 
-def find_txt():
-    for root, dirs, files in os.walk("/home/mike/Programming/albatross"):
+#d = input("Enter directory filepath:")
+
+def find_file(filetype, ext):
+    #global d
+    filelist = {}
+    for root, directories, files in os.walk('.'):
         for file in files:
-            if file.endswith((".txt")):
-                filename, file_extension = os.path.splitext(file)
+            if file.endswith(ext):
+                filename, file_ext = os.path.splitext(file)
                 pathname = os.path.join(root, file)
-                try:
+                try:#this is fucked up fix it
                     cdate = os.stat(file).st_ctime
-                    datee = date.fromtimestamp(cdate)
+                    timestamp = date.fromtimestamp(cdate)
+                    fdate = timestamp.strftime("%D")
                 except FileNotFoundError:
+                    print(f"{filename}")
                     continue 
-                txtlist.update({filename: {'Path': pathname, 'files_ext': file_extension, 'date': datee}})
+                filelist.update({filename: {'Path': pathname, 'File Extension': file_ext, 'Date': fdate}})
+    filegroup = f"{filetype}.json"
+    with open(filegroup, 'w') as json_file:
+        json.dump(filelist, json_file, sort_keys=True, indent=4)
+    
 
-find_txt()
 
-with open('test.json', 'w') as json_file:
-    json.dump(txtlist, json_file, sort_keys=True, indent=4)
+
+
+for k, v in FileTypes.items(): 
+    find_file(k, v)
